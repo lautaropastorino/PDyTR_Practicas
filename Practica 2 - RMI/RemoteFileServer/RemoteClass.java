75a35a40c46fed7b7f593d08db861b3bebbf412d
Montes, Matias Ezequiel <mati.montes@hotmail.com>
2020-10-09 19:04:02 -0300

    Primer versión leer sv

--- Practica 2 - RMI/RemoteFileServer/RemoteClass.java
+++ Practica 2 - RMI/RemoteFileServer/RemoteClass.java
@@ -7,6 +7,7 @@
 /* Needed for implementing remote method/s */
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.io.File;
 /* This class implements the interface with remote methods */
 public class RemoteClass extends UnicastRemoteObject implements IfaceRemoteClass
 {
@@ -16,11 +17,25 @@ public class RemoteClass extends UnicastRemoteObject implements IfaceRemoteClass
 	}
 	/* Remote methods implementation */
 	public byte[] readFile(String filename, int position, int bytelength) throws RemoteException
-	{
-
+	{	
+		try {
+			//IMPLEMENTAR DIRECCIÓN BASE
+			File file = new File(filename);
+			int filesize = getFileSizeBytes(file);
+			if (filesize >= position) {
+				to = Math.min(filesize, (bytelength + position));
+		      	byte[] data = Arrays.copyOfRange(Files.readAllBytes(file.toPath()), position, to);
+			}
+		    else {
+				byte[] data = new byte[0];
+		    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return data
 	}
 	public int writeFile(String filename, int bytelength, byte[] data) throws RemoteException
 	{
-		
+
 	}
 }
