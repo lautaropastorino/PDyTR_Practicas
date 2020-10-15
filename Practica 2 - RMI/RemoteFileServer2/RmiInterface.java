6fb0741ccfcbe08dcc256edf001dfb8d929b0545
lautaropastorino <lautaro.pastorino@hotmail.com>
2020-10-15 16:32:12 -0300

    Borro archivos que ya no necesitamos

--- Practica 2 - RMI/RemoteFileServer2/RmiInterface.java
+++
@@ -1,9 +0,0 @@
-//This class interfaces directly between the client and the server
-import java.rmi.*;
-import java.rmi.registry.*;
-public interface RmiInterface extends Remote {
-
-	public byte[] downloadFromServer(String filename, int position, int bytelength) throws RemoteException;
-	public int uploadToServer(String filename, int bytelength, byte[] data) throws RemoteException;
-
-}
