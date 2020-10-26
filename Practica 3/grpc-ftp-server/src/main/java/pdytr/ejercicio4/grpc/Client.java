a284020b34730180abc1f84f78a100e749ee91b4
lautaropastorino <lautaro.pastorino@hotmail.com>
2020-10-26 16:23:42 -0300

    Organicé la clase cliente para pode seleccionar operación de lectura y de escritura

--- Practica 3/grpc-ftp-server/src/main/java/pdytr/ejercicio4/grpc/Client.java
+++ Practica 3/grpc-ftp-server/src/main/java/pdytr/ejercicio4/grpc/Client.java
@@ -10,47 +10,62 @@ import io.grpc.ejercicio4.ftp.*;
 public class Client {
 
     public static void errorDeParametros() {
-        System.out.println(String.format("Uso:%n-> -Dexec.args=\"direccion puerto archivo posicion bytesALeer\""));
+        System.out.println(String.format("Uso:%n-> -Dexec.args=\"direccion puerto operacion archivo posicion bytesALeer\""));
         System.exit(1);
+    }
+
+    public static void leer(String direccion, String puerto, String archivo, int posicion, int bytesALeer) {
+        final ManagedChannel channel = ManagedChannelBuilder.forTarget(String.format("%s:%s", direccion, puerto))
+            .usePlaintext()
+            .build();
+
+        FtpServiceGrpc.FtpServiceBlockingStub stub = FtpServiceGrpc.newBlockingStub(channel);
+        LeerRequest request = LeerRequest.newBuilder()
+            .setArchivo(archivo)
+            .setPosicion(posicion)
+            .setBytesALeer(bytesALeer)
+            .build();
+
+        System.out.println("Iniciando consulta");
+
+
+        Iterator<LeerResponse> response = stub.leer(request);
+
+        int cont = 1;
+        while (response.hasNext()) {
+            LeerResponse r = response.next();
+            System.out.println("Lectura N: " + cont + " - Bytes leidos: " + r.getBytesLeidos());
+            cont++;
+        }
+
+
+        channel.shutdownNow();
+    }
+
+    public static void escribir(String direccion, String puerto, String archivo, int posicion, int bytesALeer) {
+        System.out.println("Escribir");
     }
 
     public static void main( String[] args ) throws Exception
     {
-       if (args.length != 5) {
+       if (args.length != 6) {
            errorDeParametros();
        }
        
        String direccion = args[0];
        String puerto = args[1];
-       String archivo = args[2];
-       int posicion = Integer.parseInt(args[3]);
-       int bytesALeer = Integer.parseInt(args[4]);
+       String operacion = args[2];
+       String archivo = args[3];
+       int posicion = Integer.parseInt(args[4]);
+       int bytesALeer = Integer.parseInt(args[5]);
     
-
-      final ManagedChannel channel = ManagedChannelBuilder.forTarget(String.format("%s:%s", direccion, puerto))
-        .usePlaintext()
-        .build();
-
-      FtpServiceGrpc.FtpServiceBlockingStub stub = FtpServiceGrpc.newBlockingStub(channel);
-      LeerRequest request = LeerRequest.newBuilder()
-        .setArchivo(archivo)
-        .setPosicion(posicion)
-        .setBytesALeer(bytesALeer)
-        .build();
-
-      System.out.println("Iniciando consulta");
+       if (operacion.equals("leer")) {
+            leer(direccion, puerto, archivo, posicion, bytesALeer);
+       } else if (operacion.equals("escribir")) {
+            escribir(direccion, puerto, archivo, posicion, bytesALeer);
+       } else {
+           errorDeParametros();
+       }
       
-      
-      Iterator<LeerResponse> response = stub.leer(request);
-    
-      int cont = 1;
-      while (response.hasNext()) {
-        LeerResponse r = response.next();
-        System.out.println("Lectura N: " + cont + " - Bytes leidos: " + r.getBytesLeidos());
-        cont++;
-      }
-      
-
-      channel.shutdownNow();
     }
 }
